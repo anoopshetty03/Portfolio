544a3e5b079773d25cac70d13dac3cd160c7d393
anoopshetty03 <shettyanoop2003@gmail.com>
2024-09-06 17:25:52 +0530

    data changes made

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,6 @@
 import JobZeal from '../assets/projects/JobZeal.jpeg'
+import CMS from '../assets/projects/CMS.jpg'
+import QuickChat from '../assets/projects/QuickChat.jpg'
 import MediLink from '../assets/projects/MediLink.jpeg'
 import CodeCast from '../assets/projects/CodeCast.jpg'
 
@@ -12,13 +14,27 @@ export const data=[
     },
     {
         id:2,
+        name:"College Management System",
+        image:CMS,
+        github:"https://github.com/anoopshetty03/CMS",
+        // live:"",
+    },
+    {
+        id:3,
+        name:"Real time Chat Application",
+        image:QuickChat,
+        github:"https://github.com/anoopshetty03/QuickChat",
+        // live:"",
+    },
+    {
+        id:4,
         name:"Doctor Appointment App",
         image:MediLink,
         github:"https://github.com/anoopshetty03/MediLink",
         // live:"",
     },
     {
-        id:3,
+        id:5,
         name:"Real time code collab App",
         image:CodeCast,
         github:"https://github.com/anoopshetty03/CodeCast",
